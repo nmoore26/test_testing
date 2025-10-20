caf501d0d10980a20f0af36e11f188e4a660e9c2
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-20 12:41:02 -0500

    Fix assertion syntax in test_squared.py

--- test_squared.py
+++ test_squared.py
@@ -1,9 +1,9 @@
 from squared import square_number
 def test_positive_number(self):
-    assertEqual(square_number(2), 4)
+    assert(square_number(2), 4)
 
 def test_negative_number(self):
-    assertEqual(square_number(-3), 9)
+    assert(square_number(-3), 9)
 
 def test_zero(self):
-    assertEqual(square_number(0), 0)
+    assert(square_number(0), 0)
