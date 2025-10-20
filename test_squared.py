37e535cb53b3e9a23be7681915e65c8943ae3e53
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-20 12:42:21 -0500

    Refactor test functions for square_number

--- test_squared.py
+++ test_squared.py
@@ -1,9 +1,11 @@
 from squared import square_number
-def test_positive_number(self):
-    assert(square_number(2), 4)
+def test_positive_number():
+    assert square_number(2) == 4
 
-def test_negative_number(self):
-    assert(square_number(-3), 9)
 
-def test_zero(self):
-    assert(square_number(0), 0)
+def test_negative_number():
+    assert square_number(-3) == 9
+
+
+def test_zero():
+    assert square_number(0) == 0
