3c31cd000c14a3e8e34b6fbf525d1ef2c7594af6
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-20 12:36:49 -0500

    Add tests for square_number function

---
+++ test_squared.py
@@ -0,0 +1,9 @@
+from squared import square_number
+    def test_positive_number(self):
+        self.assertEqual(square_number(2), 4)
+
+    def test_negative_number(self):
+        self.assertEqual(square_number(-3), 9)
+
+    def test_zero(self):
+        self.assertEqual(square_number(0), 0)
