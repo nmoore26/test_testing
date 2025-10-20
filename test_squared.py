64950297587203cec38f4b4462a4014a6377909b
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-20 12:39:38 -0500

    Replace assertEqual with self.assertEqual

--- test_squared.py
+++ test_squared.py
@@ -1,9 +1,9 @@
 from squared import square_number
 def test_positive_number(self):
-    self.assertEqual(square_number(2), 4)
+    assertEqual(square_number(2), 4)
 
 def test_negative_number(self):
-    self.assertEqual(square_number(-3), 9)
+    assertEqual(square_number(-3), 9)
 
 def test_zero(self):
-    self.assertEqual(square_number(0), 0)
+    assertEqual(square_number(0), 0)
